8dfb96b847a4a1a546bc3461b44f3da2c9775551
Andrew Polak <z3330012@ad.unsw.edu.au>
2024-12-21 23:12:26 +1100

    chore: add session02 materials

--- session02/type_conversion.py
+++ session02/type_conversion.py
@@ -5,10 +5,13 @@ def main():
     # 2. Convert it to int.
     # 3. Add 10.
     # 4. Print the result.
-    number_str = input()
-    number = int(number_str)
-    number = number + 10
-    print(number)
+
+    # number_str = input()
+    # number = int(number_str)
+    # number = number + 10
+    # print(number)
+
+    print(int(input()) + 10)
     pass
 
 if __name__ == "__main__":
